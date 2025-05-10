608c387cbbebde90f4c396415ef916c7b53797fa
arafatDU <bsse1435@iit.du.ac.bd>
2025-05-10 20:02:52 +0600

    Navbar  created

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
-import './App.css'
 import './index.css'
 import { LoadingScreen } from './components/LoadingScreen'
 import { useEffect, useState } from "react";
+import { Navbar } from './components/Navbar';
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
 
   return (
     <>
-      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />} {" "}
+      <div
+        className={`min-h-screen w-full transition-opacity duration-700 ${
+          isLoaded ? "opacity-100" : "opacity-0"
+        } bg-black text-gray-100`}
+      >
+        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+
+      </div>
     </>
   )
 }
