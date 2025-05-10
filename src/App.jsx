a104edfc7d756145367f16236bfd12ea403bb9d4
arafatDU <bsse1435@iit.du.ac.bd>
2025-05-10 19:30:56 +0600

    loadin animation <Hello World /> done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import './index.css'
+import { LoadingScreen } from './components/LoadingScreen'
+import { useEffect, useState } from "react";
 
 function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
 
   return (
     <>
-
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
     </>
   )
 }
